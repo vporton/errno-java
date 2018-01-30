88e731eac1a153f9c64abc0d835ac3638996ab59
Victor Porton <porton@narod.ru>
2018-01-30 21:07:53 +0200

    directory structure

---
+++ src/test/org/portonvictor/errno/test/PrintError.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.portonvictor.errno.test;
+
+/**
+ *
+ * @author porton
+ */
+public class PrintError {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Error err = args.length != 0 ? new org.portonvictor.errno.Errno(Integer.parseInt(args[0]))
+                                     : new org.portonvictor.errno.Errno();
+        System.out.println(err.getMessage());
+    }
+
+}
